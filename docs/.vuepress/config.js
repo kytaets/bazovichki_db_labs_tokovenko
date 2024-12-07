e2e686585e0819e63ebddfbd1b838f183c0e16cc
kytaec <vladislavtokovenko4@gmail.com>
2024-12-07 18:06:23 +0200

    fsd

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     }]
   ],
   port: 3030,
-  base: '/bazovichki_db_labs/',
+  base: '/db_course_work/',
   theme: 'cool',
   // dest: 'dist',
   head: [
@@ -74,7 +74,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/Crosshell/bazovichki_db_labs',
+    repo: 'https://github.com/kytaets/db_course_work',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
